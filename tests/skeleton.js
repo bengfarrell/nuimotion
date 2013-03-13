7d14e077c8114884a1df1ad7dc444f9e08af3e2e
Ben Farrell <ben@benfarrell.com>
2013-03-12 23:04:49 -0400

    added tests

--- tests/skeleton.js
+++ tests/skeleton.js
@@ -1,7 +1,32 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ben
- * Date: 3/12/13
- * Time: 8:56 PM
- * To change this template use File | Settings | File Templates.
- */
+var nui = require("nuimotion");
+
+var frameLoop;
+var frameDelay = 50; /* milliseconds between frame loop iterations */
+
+nui.context.on = function(name) {
+    switch (name) {
+        case "SKELETON_TRACKING":
+            if (!frameLoop) {
+                console.log("Skeleton is tracking");
+                frameLoop = setInterval(onUpdate,frameDelay);
+            }
+            break;
+
+        case "SKELETON_STOPPED_TRACKING":
+            if (frameLoop) {
+                console.log("Skeleton stopped tracking");
+                clearInterval(frameLoop);
+                frameLoop = null;
+            }
+    }
+};
+
+process.on('exit', function() {
+    nui.close();
+});
+
+function onUpdate() {
+   console.log(nui.getJoints("left_shoulder").left_shoulder.xRotation )
+}
+
+nui.init();
