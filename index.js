591c379baa99a1c374e93eff61425ba361ebc980
Ben Farrell <ben@benfarrell.com>
2013-03-12 23:12:51 -0400

    added some enumeration to avoid syntax errors when requesting joints

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 module.exports = require('./src/build/Release/nuimotion.node');
+
+Joints = {};
+Joints.JOINT_LEFT_HAND = "left_hand";
+Joints.JOINT_RIGHT_HAND = "right_hand";
+
+Joints.JOINT_LEFT_ELBOW = "left_elbow";
+Joints.JOINT_RIGHT_ELBOW = "right_elbow";
+
+Joints.JOINT_LEFT_SHOULDER = "left_shoulder";
+Joints.JOINT_RIGHT_SHOULDER = "right_shoulder";
+
+Joints.JOINT_LEFT_HIP = "left_hip";
+Joints.JOINT_RIGHT_HIP = "right_hip";
+
+Joints.TORSO = "torso";
+Joints.HEAD = "head";
