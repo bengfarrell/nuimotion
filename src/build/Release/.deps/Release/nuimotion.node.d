014213e51bd57f4811fa8da2a773a3a41d8b2eb6
Ben Farrell <ben@benfarrell.com>
2013-03-16 21:07:13 -0400

    remove build files

--- src/build/Release/.deps/Release/nuimotion.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/nuimotion.node := ln -f "Release/obj.target/nuimotion.node" "Release/nuimotion.node" 2>/dev/null || (rm -rf "Release/nuimotion.node" && cp -af "Release/obj.target/nuimotion.node" "Release/nuimotion.node")
